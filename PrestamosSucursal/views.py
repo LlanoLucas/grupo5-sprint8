80eb6381c6335db62b51a311069e9cedef8330a2
OliviaRolando <105291105+OliviaRolando@users.noreply.github.com>
2022-09-11 15:27:04 -0300

    Add files via upload

---
+++ PrestamosSucursal/views.py
@@ -0,0 +1,18 @@
+from .models import Prestamo
+from .serializers import PrestamosSerializers
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+# Create your views here.
+
+class PrestamoList(APIView, LoginRequiredMixin):
+    def get(self, request, branch_id):
+        user = User.objects.get(username = User.get_username(request.user))
+        prestamos = Prestamo.objects.filter(branch_id = branch_id)
+        serializer = PrestamosSerializers(prestamos, many = True)
+        if user.is_staff == True:
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
